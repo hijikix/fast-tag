2ced4dbf976be02ae3c7b010a33e1b78431db0c0
Daisuke Hashimoto <galapagosit@gmail.com>
2025-05-15 17:27:23 +0900

    multi_pages

---
+++ src/pages/detail.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+pub fn setup() {
+    println!("detail setup");
+}
+
+pub fn update() {
+    println!("detail update");
+}
+
+pub fn cleanup() {
+    println!("detail cleanup");
+}
