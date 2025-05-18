54f3c2fb58cfe644f655a62a061bbb31995fe60a
Daisuke Hashimoto <galapagosit@gmail.com>
2025-05-18 16:31:28 +0900

    show_image

--- src/pages/detail.rs
+++ src/pages/detail.rs
@@ -1,13 +1,57 @@
-use bevy::prelude::*;
+use bevy::{asset::RenderAssetUsages, prelude::*};
 
 #[derive(Resource, Default)]
 pub struct Parameters {
     pub url: String,
 }
 
-pub fn setup(params: Res<Parameters>) {
+fn load_image(url: &str) -> Result<image::DynamicImage, image::ImageError> {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let image_bytes = rt.block_on(async {
+        let response = match reqwest::get(url).await {
+            Ok(response) => response,
+            Err(e) => {
+                eprintln!("image download error: {}", e);
+                return None;
+            }
+        };
+
+        match response.bytes().await {
+            Ok(bytes) => Some(bytes.to_vec()),
+            Err(e) => {
+                eprintln!("image bytes error: {}", e);
+                None
+            }
+        }
+    });
+
+    if let Some(bytes) = image_bytes {
+        let image = image::load_from_memory(&bytes)?;
+        println!("image loaded!!!");
+        return Ok(image);
+    }
+
+    Err(image::ImageError::Unsupported(
+        image::error::UnsupportedError::from_format_and_kind(
+            image::error::ImageFormatHint::Unknown,
+            image::error::UnsupportedErrorKind::Format(image::error::ImageFormatHint::Unknown),
+        ),
+    ))
+}
+
+pub fn setup(mut commands: Commands, params: Res<Parameters>, mut images: ResMut<Assets<Image>>) {
     println!("detail setup");
     println!("url {:?}", params.url);
+    let dynamic_image = match load_image(&params.url) {
+        Ok(response) => response,
+        Err(e) => {
+            eprintln!("load_image error: {}", e);
+            return;
+        }
+    };
+    let image = Image::from_dynamic(dynamic_image, true, RenderAssetUsages::default());
+    let image_handle = images.add(image);
+    commands.spawn(Sprite::from_image(image_handle));
 }
 
 pub fn update() {
