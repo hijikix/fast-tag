31c621e51a150a3c0f0e135fca4b4e1d1e1caf54
Daisuke Hashimoto <galapagosit@gmail.com>
2025-05-24 00:53:22 +0900

    draw_rect

--- src/pages/detail.rs
+++ src/pages/detail.rs
@@ -1,6 +1,9 @@
 use crate::pages::components::egui_common;
 use crate::state::AppState;
 use bevy::asset::RenderAssetUsages;
+use bevy::color::palettes::css::*;
+use bevy::input::ButtonState;
+use bevy::input::mouse::MouseButtonInput;
 use bevy::prelude::*;
 use bevy_egui::EguiContexts;
 
@@ -46,10 +49,20 @@ fn load_image(url: &str) -> Result<image::DynamicImage, image::ImageError> {
 #[derive(Resource)]
 pub struct DetailData {
     image_entity: Entity,
+    cursor_posision: Option<Vec2>,
+    start_position: Option<Vec2>,
+    rectangles: Vec<(Vec2, Vec2)>,
 }
 
-pub fn setup(mut commands: Commands, params: Res<Parameters>, mut images: ResMut<Assets<Image>>) {
+pub fn setup(
+    mut commands: Commands,
+    params: Res<Parameters>,
+    mut images: ResMut<Assets<Image>>,
+    mut config_store: ResMut<GizmoConfigStore>,
+) {
     println!("detail setup");
+
+    // load image
     println!("url {:?}", params.url);
     let dynamic_image = match load_image(&params.url) {
         Ok(response) => response,
@@ -61,10 +74,67 @@ pub fn setup(mut commands: Commands, params: Res<Parameters>, mut images: ResMut
     let image = Image::from_dynamic(dynamic_image, true, RenderAssetUsages::default());
     let image_handle = images.add(image);
     let image_entity = commands.spawn(Sprite::from_image(image_handle)).id();
-    commands.insert_resource(DetailData { image_entity });
+
+    // gizmo config
+    let (config, _) = config_store.config_mut::<DefaultGizmoConfigGroup>();
+    config.line.width = 3.;
+
+    // add resource
+    commands.insert_resource(DetailData {
+        image_entity,
+        cursor_posision: None,
+        start_position: None,
+        rectangles: Vec::new(),
+    });
 }
 
-pub fn update() {}
+pub fn update(
+    cameras: Query<(&Camera, &GlobalTransform)>,
+    mut gizmos: Gizmos,
+    mut detail_data: ResMut<DetailData>,
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+) {
+    let (camera, camera_transform) = cameras.single().unwrap();
+    for event in cursor_moved_events.read() {
+        // move to world pos
+        detail_data.cursor_posision = camera
+            .viewport_to_world_2d(camera_transform, event.position)
+            .ok();
+    }
+
+    for event in mouse_button_input_events.read() {
+        info!("{:?}", event);
+        if event.button == MouseButton::Left && event.state == ButtonState::Pressed {
+            detail_data.start_position = detail_data.cursor_posision;
+        }
+
+        if detail_data.start_position.is_some()
+            && event.button == MouseButton::Left
+            && event.state == ButtonState::Released
+        {
+            // Store the positions in temporary variables to avoid simultaneous borrows
+            let start_pos = detail_data.start_position.unwrap();
+            let end_pos = detail_data.cursor_posision.unwrap();
+
+            // Now push to rectangles using the temporary variables
+            detail_data.rectangles.push((start_pos, end_pos));
+            detail_data.start_position = None;
+        }
+    }
+
+    // dragging rect
+    if detail_data.start_position.is_some() {
+        let start_pos = detail_data.start_position.unwrap();
+        let end_pos = detail_data.cursor_posision.unwrap();
+        gizmos.rect_2d((start_pos + end_pos) / 2.0, end_pos - start_pos, RED);
+    }
+
+    // Draw all stored rectangles
+    for (start_pos, end_pos) in &detail_data.rectangles {
+        gizmos.rect_2d((start_pos + end_pos) / 2.0, end_pos - start_pos, RED);
+    }
+}
 
 pub fn ui_system(
     mut contexts: EguiContexts,
